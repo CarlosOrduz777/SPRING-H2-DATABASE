f68dca6cd36d0086a81095cae63c4a87ee699429
CarlosOrduz777 <carlos.orduz@mail.escuelaing.edu.co>
2023-02-28 21:00:46 -0500

    h2 database created

---
+++ src/main/java/com/example/data/h2DataBase/H2DataBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.data.h2DataBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2DataBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2DataBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/data/h2DataBase/H2DataBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.data.h2DataBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2DataBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
